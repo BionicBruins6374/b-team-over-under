b20875f2406518662d8b5846665633c8004d78e4
zoya <zdzhnou@gmail.com>
2023-10-27 13:01:33 -0700

    speed change v1

--- include/Flywheel.hpp
+++ include/Flywheel.hpp
@@ -1,3 +1,23 @@
 #pragma once
 
 #include "main.h"
+
+
+
+class Flywheel {
+    private:
+    // TODO: test and change values
+        enum Speed {
+            high = 11000,
+            low = 7000
+        };
+        bool powerState = false; // false is off, true is on
+        Speed speed = high; // high speed is true, low speed is false; will only be considered when Flywheel on
+        Motor m_left;   
+        int8_t polarity = -1; // -1 = deintaking, 1 = shooting; TODO: change default value?     
+    public:
+        Flywheel(uint8_t m_left_port);
+        void toggle();
+        void speed_change(); 
+        void change_direction();
+};
---
+++ src/Flywheel.cpp
@@ -0,0 +1,27 @@
+#include "Flywheel.hpp"
+
+// TODO: only allows speed change if motor is on; change behavior?
+// other options include: turning off flywheel sets speed back to high by default, but pressing the speed change 
+//   button would turn on the flywheel with the low speed
+void Flywheel::speed_change() {
+    if (powerState) {
+        switch (speed)
+        {
+        case high:
+            speed = low;
+            break;
+        case low: 
+            speed = high;
+            break;
+        }
+    }
+    m_left.move_voltage(speed  * polarity);
+}
+
+void Flywheel::toggle() {
+    
+}
+
+void Flywheel::change_direction() {
+
+}

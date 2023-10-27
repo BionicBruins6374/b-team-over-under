e8d1f0fedbc71cc0f23b21503c0d6168094aad0d
zoya <zdzhnou@gmail.com>
2023-10-27 02:04:02 -0700

    added Drivetrain constructor

--- src/Drivetrain.cpp
+++ src/Drivetrain.cpp
@@ -1,6 +1,18 @@
 #include "Drivetrain.hpp"
 #include "constants.hpp"
 
+// reversed motors depend on motor placement 
+Drivetrain::Drivetrain(uint8_t const left_back_motor_port, uint8_t const right_back_motor_port, uint8_t const left_front_motor_port, uint8_t const right_front_motor_port, uint8_t const right_middle_motor_port, uint8_t const left_middle_motor_port)
+    : m_left_back_motor{ left_back_motor_port, true }
+    , m_right_back_motor{ right_back_motor_port }
+    , m_left_front_motor{ left_front_motor_port, true }
+    , m_right_front_motor{ right_front_motor_port}
+    , m_left_middle_motor{ left_middle_motor_port, true }
+    , m_right_middle_motor{ right_middle_motor_port }
+	, m_drive_turn_sensitivity{constants::drive_turn_sensitivity}
+{
+}
+
 // scales input from joysticks so that when joystick
 // is closed to the center, the bot moves less
 static float scale(float const raw)

a2966199c3747fb33f07edc7d304caff95c1eb79
zoya <zdzhnou@gmail.com>
2024-01-03 05:01:40 -0800

    robot.hpp finished

---
+++ include/Robot.hpp
@@ -0,0 +1,19 @@
+#include "api.h"
+#include "Cata.hpp"
+#include "Intake.hpp"
+#include "Wings.hpp"
+
+class Robot {
+    private:
+        pros::Controller m_controller (pros::E_CONTROLLER_MASTER); 
+
+        Intake intake; 
+        Catapult cata;
+        Wings wings;
+        
+        void update_intake();
+        void update_cata(); 
+        void update_wings(); 
+    public: 
+        void update(std::string[] info); 
+}

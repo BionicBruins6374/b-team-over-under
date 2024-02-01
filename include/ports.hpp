db1d4ea624650e1d3f206149c2832096ea0b6747
zoya <zdzhnou@gmail.com>
2024-01-31 18:59:31 -0800

    ports change

--- include/ports.hpp
+++ include/ports.hpp
@@ -2,18 +2,18 @@
 
 namespace ports {
 
-    constexpr int8_t LEFT_FRONT_DT = 1;
-    constexpr int8_t LEFT_BACK_BOTTOM_DT = 2;
-    constexpr int8_t LEFT_BACK_TOP_DT = -4
+    constexpr int8_t LEFT_BACK_DT = 8;
+    constexpr int8_t LEFT_FRONT_BOTTOM_DT = 16;
+    constexpr int8_t LEFT_FRONT_TOP_DT = -5
     ; // needs to be fixed 
 
-    constexpr int8_t RIGHT_FRONT_DT = -8;
-    constexpr int8_t RIGHT_BACK_BOTTOM_DT = -9;
-    constexpr int8_t RIGHT_BACK_TOP_DT = 10;
+    constexpr int8_t RIGHT_BACK_DT = -7;
+    constexpr int8_t RIGHT_FRONT_BOTTOM_DT = -6;
+    constexpr int8_t RIGHT_FRONT_TOP_DT = 19;
 
-    constexpr int8_t INTAKE_MOTOR = 6; 
-    constexpr int8_t BIG_CATAPULT_MOTOR = 7;
-    constexpr int8_t SMALL_CATAPULT_MOTOR = 5;
+    constexpr int8_t INTAKE_MOTOR = 18; 
+    constexpr int8_t BIG_CATAPULT_MOTOR = 17;
+    constexpr int8_t SMALL_CATAPULT_MOTOR = -12;
 
     constexpr uint8_t WING_PORT_RIGHT = 'G'; 
     constexpr uint8_t WING_PORT_LEFT = 'H'; 
